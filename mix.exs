d7d6fae43116e1dd5fff97b8df8a871012d3a4e2
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2022-11-04 22:37:53 +0100

    Refactor project configuration

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule Trolleybus.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/sketch-hq/trolleybus"
+
   def project do
     [
       app: :trolleybus,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -18,10 +23,33 @@ defmodule Trolleybus.MixProject do
     ]
   end
 
+  defp package do
+    [
+      description: "Local, application-level PubSub API for dispatching side effects.",
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
   defp deps do
     [
-      {:ecto, "~> 3.0", optional: true},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:ecto, "~> 3.0"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ]
     ]
   end
 end
