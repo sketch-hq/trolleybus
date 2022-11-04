5efbdac0125b043090944884dacf1e3ac07e1b7c
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2022-11-04 22:56:05 +0100

    Clean up credo config slightly

--- .credo.exs
+++ .credo.exs
@@ -1,9 +1,3 @@
-# This file contains the configuration for Credo and you are probably reading
-# this after creating it with `mix credo.gen.config`.
-#
-# If you find anything wrong or unclear in this file, please report an
-# issue on GitHub: https://github.com/rrrene/credo/issues
-#
 %{
   #
   # You can have as many configs as you like in the `configs:` field.
