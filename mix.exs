9f66e53099487a3e0f62815159735693f6665372
dgvncsz0f <dgvncsz0f@gmail.com>
2025-07-17 10:08:13 -0300

    remove support for elixir-1.12

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Trolleybus.MixProject do
     [
       app: :trolleybus,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
